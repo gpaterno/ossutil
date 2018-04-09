7bdcef763d8b3c48931cdf2f1b7c8d8a5910da4a
佐钊 <zubo.yzb@alibaba-inc.com>
2018-04-09 14:17:25 +0800

    Limit auto-calculated parallel and routines Add prompt for NoSuchUpload

--- ossutil.go
+++ ossutil.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/aliyun/ossutil/lib"
 )
@@ -10,6 +11,9 @@ import (
 func main() {
 	if err := lib.ParseAndRunCommand(); err != nil {
 		fmt.Printf("Error: %s!\n", err)
+		if strings.Contains(err.Error(), "ErrorCode=NoSuchUpload") {
+			fmt.Printf("Please remove checkpoint dir '%s' before upload this file again", lib.CheckpointDir)
+		}
 		os.Exit(1)
 	}
 	os.Exit(0)
